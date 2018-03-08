ab40a02e5e2651681dd0327663590ddfffedc393
Tasos Katsoulas <akatsoulas@gmail.com>
2018-03-08 20:03:13 +0200

    Various fixes.
    * Fix RadioSelect's horizontal render.
    * Small updates for Django 1.11.

--- mozillians/users/managers.py
+++ mozillians/users/managers.py
@@ -1,6 +1,6 @@
 from django.apps import apps
 from django.db.models import Q, Manager
-from django.db.models.query import QuerySet, ValuesQuerySet
+from django.db.models.query import QuerySet
 
 from django.utils.translation import ugettext_lazy as _lazy
 
@@ -18,41 +18,41 @@ PRIVACY_CHOICES_WITH_PRIVATE = ((MOZILLIANS, _lazy(u'Mozillians')),
 PUBLIC_INDEXABLE_FIELDS = ['full_name', 'ircname', 'email']
 
 
-class UserProfileValuesQuerySet(ValuesQuerySet):
-    """Custom ValuesQuerySet to support privacy.
+# class UserProfileValuesQuerySet(ValuesQuerySet):
+#     """Custom ValuesQuerySet to support privacy.
 
-    Note that when you specify fields in values() you need to include
-    the related privacy field in your query.
+#     Note that when you specify fields in values() you need to include
+#     the related privacy field in your query.
 
-    E.g. .values('first_name', 'privacy_first_name')
+#     E.g. .values('first_name', 'privacy_first_name')
 
-    """
+#     """
 
-    def _clone(self, *args, **kwargs):
-        c = super(UserProfileValuesQuerySet, self)._clone(*args, **kwargs)
-        c._privacy_level = getattr(self, '_privacy_level', None)
-        return c
+#     def _clone(self, *args, **kwargs):
+#         c = super(UserProfileValuesQuerySet, self)._clone(*args, **kwargs)
+#         c._privacy_level = getattr(self, '_privacy_level', None)
+#         return c
 
-    def iterator(self):
-        # Purge any extra columns that haven't been explicitly asked for
-        extra_names = self.query.extra_select.keys()
-        field_names = self.field_names
-        aggregate_names = self.query.aggregate_select.keys()
+#     def iterator(self):
+#         # Purge any extra columns that haven't been explicitly asked for
+#         extra_names = self.query.extra_select.keys()
+#         field_names = self.field_names
+#         aggregate_names = self.query.aggregate_select.keys()
 
-        names = extra_names + field_names + aggregate_names
+#         names = extra_names + field_names + aggregate_names
 
-        model_privacy_fields = self.model.privacy_fields()
+#         model_privacy_fields = self.model.privacy_fields()
 
-        privacy_fields = [
-            (names.index('privacy_%s' % field), names.index(field), field)
-            for field in set(model_privacy_fields) & set(names)]
+#         privacy_fields = [
+#             (names.index('privacy_%s' % field), names.index(field), field)
+#             for field in set(model_privacy_fields) & set(names)]
 
-        for row in self.query.get_compiler(self.db).results_iter():
-            row = list(row)
-            for levelindex, fieldindex, field in privacy_fields:
-                if row[levelindex] < self._privacy_level:
-                    row[fieldindex] = model_privacy_fields[field]
-            yield dict(zip(names, row))
+#         for row in self.query.get_compiler(self.db).results_iter():
+#             row = list(row)
+#             for levelindex, fieldindex, field in privacy_fields:
+#                 if row[levelindex] < self._privacy_level:
+#                     row[fieldindex] = model_privacy_fields[field]
+#             yield dict(zip(names, row))
 
 
 class UserProfileQuerySet(QuerySet):
@@ -101,8 +101,8 @@ class UserProfileQuerySet(QuerySet):
 
     def _clone(self, *args, **kwargs):
         """Custom _clone with privacy level propagation."""
-        if kwargs.get('klass', None) == ValuesQuerySet:
-            kwargs['klass'] = UserProfileValuesQuerySet
+        # if kwargs.get('klass', None) == ValuesQuerySet:
+        #     kwargs['klass'] = UserProfileValuesQuerySet
         c = super(UserProfileQuerySet, self)._clone(*args, **kwargs)
         c._privacy_level = getattr(self, '_privacy_level', None)
         return c
