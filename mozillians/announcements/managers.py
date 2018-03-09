d7c817ba042717a4f345fe2afca9aa3e42dd8c55
Tasos Katsoulas <akatsoulas@gmail.com>
2018-03-09 22:00:13 +0200

    Fix tests.
    * Drop old API, get_field_by_name, from Model._meta.
    * Do not use naive datetime objects in the announcements app.

--- mozillians/announcements/managers.py
+++ mozillians/announcements/managers.py
@@ -1,7 +1,6 @@
-from datetime import datetime
-
 from django.db import models
 from django.db.models import Q
+from django.utils.timezone import now
 
 
 class AnnouncementManager(models.Manager):
@@ -10,15 +9,13 @@ class AnnouncementManager(models.Manager):
 
     def published(self):
         """Return published announcements."""
-        now = datetime.now()
-        return (self.filter(publish_from__lte=now)
+        return (self.filter(publish_from__lte=now())
                     .filter(Q(publish_until__isnull=True) |
                             (Q(publish_until__isnull=False) &
-                             Q(publish_until__gt=now))))
+                             Q(publish_until__gt=now()))))
 
     def unpublished(self):
         """Return unpublished announcements."""
-        now = datetime.now()
-        return self.filter(Q(publish_from__gt=now) |
+        return self.filter(Q(publish_from__gt=now()) |
                            (Q(publish_until__isnull=False) &
-                            Q(publish_until__lte=now)))
+                            Q(publish_until__lte=now())))
